130ecf488d236c1db79ac048ce44002bbe529ac9
fahim-shahriar221 <shahriar.fahim221b@gmail.com>
2024-09-14 20:05:13 +0600

    Sum of all elements of an array

---
+++ SumOfArray.java
@@ -0,0 +1,12 @@
+package com.myprojects;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,6,11,31};
+        int sum=0;
+        for(int i=0; i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println("Sum of elements: "+sum);
+    }
+}
