b6214d528246f8addfe81ec17c0e4a50e3c4d506
fahim-shahriar221 <shahriar.fahim221b@gmail.com>
2024-09-19 19:28:24 +0600

    Multiplication Table

---
+++ Table.java
@@ -0,0 +1,10 @@
+package com.myprojects;
+
+public class Table {
+    public static void main(String[] args) {
+        int n=11;
+        for(int i=1; i<=10; i++){
+            System.out.println(n+" X "+i+" = "+n*i);
+        }
+    }
+}
