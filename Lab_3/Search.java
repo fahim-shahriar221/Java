1e6ba3cc4630b3aa0e720bc1fb8c811372bacf73
fahim-shahriar221 <shahriar.fahim221b@gmail.com>
2025-01-05 21:04:57 +0600

    Simple Problems

---
+++ Lab_3/Search.java
@@ -0,0 +1,18 @@
+package Lab_3;
+
+public class Search {
+    public static void main(String[] args) {
+        int [] arr={31,11,1,4,2,6,3};
+        int i;
+        System.out.println("Array elements are: ");
+        for(i=0; i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        int x=6;
+        for(i=0;i< arr.length;i++){
+            if (arr[i]==x) {
+                System.out.println("\n"+x+" found at index: "+i);
+            }
+        }
+    }
+}
